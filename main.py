c6fb145a2b0b7c4f4a2134cdba29a1cb213c3d50
whitej68 <114672020+whitej68@users.noreply.github.com>
2023-10-17 18:03:16 -0400

    Added status bar with edit and delete widgets

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QLabel, QWidget, QTableWidget,
                              QGridLayout, QLineEdit, QPushButton,
                              QMainWindow, QVBoxLayout, QTableWidgetItem,
-                             QDialog, QComboBox, QToolBar)
+                             QDialog, QComboBox, QToolBar, QStatusBar)
 from PyQt6.QtGui import QAction, QIcon
 import sys
 import sqlite3
@@ -43,6 +43,30 @@ class MainWindow(QMainWindow):
         toolbar.addAction(add_student_action)
         toolbar.addAction(search_action)
 
+        # Create status bar and add status bar elements
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        # Detect a cell click
+        self.table.cellClicked.connect(self.cell_clicked)
+
+
+    def cell_clicked(self):
+        edit_button = QPushButton("Edit Record")
+        edit_button.clicked.connect(self.edit)
+
+        delete_button = QPushButton("Delete Record")
+        delete_button.clicked.connect(self.delete)
+
+        children = self.findChildren(QPushButton)
+
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+
+        self.statusbar.addWidget(edit_button)
+        self.statusbar.addWidget(delete_button)
+
     def load_data(self):
         connection = sqlite3.connect("database.db")
         result = connection.execute("SELECT * FROM students")
@@ -61,6 +85,19 @@ class MainWindow(QMainWindow):
         dialog = SearchDialog()
         dialog.exec()
 
+    def edit(self):
+        dialog = EditDialog()
+        dialog.exec
+
+    def delete(self):
+        dialog = DeleteDialog()
+        dialog.exec
+
+class EditDialog(QDialog):
+    pass
+
+class DeleteDialog(QDialog):
+    pass
 
 class InsertDialog(QDialog):
     def __init__(self):
